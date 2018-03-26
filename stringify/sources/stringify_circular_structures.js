1991dcded3c482371110fb7b1acbc4e76f31c3b9
K.C.Ashish Kumar <kcak11@gmail.com>
2018-03-26 19:51:50 +0530

    Update stringify_circular_structures.js

--- stringify/sources/stringify_circular_structures.js
+++ stringify/sources/stringify_circular_structures.js
@@ -1,33 +1,33 @@
-var getJSON = function(obj) {
-    var str = JSON.stringify(obj, function(key, value) {
-        if (value && typeof value === "object") {
-            if (!value["_[isAlreadyProcessed]_"]) {
-                value["_[isAlreadyProcessed]_"] = true;
+var JSONUtils = {
+    "getJSON": function(obj) {
+        var _cache = {};
+        var str = JSON.stringify(obj, function(key, value) {
+            if (value && typeof value === "object") {
+                if (!_cache[value]) {
+                    _cache[value] = "[.....Node.Processed.....]";
+                    return value;
+                } else {
+                    return "[.....circRef.Key.....]";
+                }
+            } else {
+                return value;
+            }
+        });
+        var _obj = JSON.parse(str);
+        return JSON.stringify(_obj, function(key, value) {
+            if (value && typeof value === "object") {
+                Object.getOwnPropertyNames(value).forEach(function(k) {
+                    if (value[k] === "[.....circRef.Key.....]") {
+                        delete value[k];
+                    }
+                });
                 return value;
             } else {
-                return "[.....circRef.Key.....]";
+                return value;
             }
-        } else {
-            return value;
-        }
-    });
-    var _obj = JSON.parse(str);
-    return JSON.stringify(_obj, function(key, value) {
-        if (value && typeof value === "object") {
-            Object.getOwnPropertyNames(value).forEach(function(k) {
-                if (value[k] === "[.....circRef.Key.....]") {
-                    delete value[k];
-                }
-            });
-            if (value["_[isAlreadyProcessed]_"]) {
-                delete value["_[isAlreadyProcessed]_"];
-            }
-            return value;
-        } else {
-            return value;
-        }
-    });
+        });
+    }
 };
 
 /* Usage / Tests */
-getJSON(obj);
+JSONUtils.getJSON(obj);
