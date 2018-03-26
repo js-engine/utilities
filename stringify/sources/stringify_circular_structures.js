5bba56d24ab50916b5b46b993e56cb4a2def9911
K.C.Ashish Kumar <kcak11@gmail.com>
2018-03-26 18:22:42 +0530

    Update stringify_circular_structures.js

--- stringify/sources/stringify_circular_structures.js
+++ stringify/sources/stringify_circular_structures.js
@@ -1,6 +1,6 @@
 var getJSON = function(obj) {
     var str = JSON.stringify(obj, function(key, value) {
-        if (typeof value === "object") {
+        if (value && typeof value === "object") {
             if (!value["_[isAlreadyProcessed]_"]) {
                 value["_[isAlreadyProcessed]_"] = true;
                 return value;
@@ -13,7 +13,7 @@ var getJSON = function(obj) {
     });
     var _obj = JSON.parse(str);
     return JSON.stringify(_obj, function(key, value) {
-        if (typeof value === "object") {
+        if (value && typeof value === "object") {
             Object.getOwnPropertyNames(value).forEach(function(k) {
                 if (value[k] === "[.....circRef.Key.....]") {
                     delete value[k];
@@ -30,21 +30,4 @@ var getJSON = function(obj) {
 };
 
 /* Usage / Tests */
-var obj = {
-    a: 5,
-    b: 5,
-    sub: {}
-};
-obj.sub.tmp = obj;
-obj.sub.tmp1 = obj;
-obj.sub.tmp2 = obj;
-obj.sub.tmp3 = obj;
-obj.sub.tt = {
-    kk: 56,
-    subsub: {}
-};
-obj.sub.tt.subsub.kpt = obj;
-obj.sub.tt.subsub.qqf = {};
-obj.sub.tt.subsub.qqf.iii = obj.sub.tt;
-
 getJSON(obj);
